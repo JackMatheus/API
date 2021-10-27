1d16139947ede91e2fbd74232654c9c24ae1f462
unknown <jack.matheusch@gmail.com>
2021-10-27 09:48:17 -0300

    API Coin Market Cap usando o metodo AJAX

---
+++ API_AJAX_COINS/README.md
@@ -0,0 +1,16 @@
+Devido a problema no cors que impossibilitou o desenvolvimento completo da aplicação via frontend usei a estratégia via Nodejs.
+
+Tecnologias Utilizadas : 
+NodeJs: 14.15.5
+Font Awesome: 5.15.3
+Bootstrap: 5.0.1
+Google Fonts
+
+bibliografia:
+https://blog.matheuscastiglioni.com.br/requisicoes-ajax-com-javascript/
+https://github.com/Marcelo-Diament/Github-API-with-AJAX/blob/master/index.html
+https://www.infowester.com/tagsdesconhecidas2.php
+
+nota: AJAX é uma aplicação para o front, não sendo necessário realizar a chamada pelo backend .
+
+
---
+++ API_AJAX_COINS/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const request = require('request');
+const rp = require('request-promise');
+
+var router = express.Router();
+
+const app = express();
+
+app.set("views", "./")
+app.set("view engine", "ejs")
+
+//const url = "https://sistemashomologacao.suafaculdade.com.br/processoseletivo/administrativopresencial/api/alunos";
+const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=4a7b2761-323b-4898-8b22-b644f200eb8e";
+let config = {
+  url: url,
+  headers: {
+    'Content-Type': 'application/data',
+    'Accept': 'application/data'
+  },
+  method: "GET"
+};
+
+app.get('/', (req, res) => {
+
+  return rp(config).then(function (data) {
+    res.render('index', {
+        // coin: JSON.parse(data)
+        
+    })
+  })
+});
+app.get('/coins', (req, res) => {
+  
+  return rp(config).then(function (data) {
+    res.json(JSON.parse(data))
+  })
+})
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta: 3000")
+});
+
+module.exports = app;
+
+
+
+
+
